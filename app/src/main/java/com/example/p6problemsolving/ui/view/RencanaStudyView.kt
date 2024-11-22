af5a247b581bffafc92f464011ad413defcbc77f
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2024-11-22 19:41:15 +0700

    Menambahkan Spacer, Column, Text, Box, dan Icon pada Column RencanaStudyView

--- app/src/main/java/com/example/p6problemsolving/ui/view/RencanaStudyView.kt
+++ app/src/main/java/com/example/p6problemsolving/ui/view/RencanaStudyView.kt
@@ -2,13 +2,19 @@ package com.example.p6problemsolving.ui.view
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.shape.CircleShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.Icon
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -17,15 +23,21 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.colorResource
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
 import com.example.p6problemsolving.R
+import com.example.p6problemsolving.ui.model.Mahasiswa
 
-@Preview(showBackground = true)
+//@Preview(showBackground = true)
 @Composable
-fun RencanaStudyView(){
+fun RencanaStudyView(
+    mahasiswa: Mahasiswa,
+) {
     var choesenDropdown by remember { mutableStateOf("") }
     var checked by remember { mutableStateOf("") }
     var pilihanKelas by remember { mutableStateOf("") }
@@ -37,12 +49,12 @@ fun RencanaStudyView(){
             .fillMaxSize()
             .background(color = colorResource(id = R.color.primary))
     ) {
-        Row (
+        Row(
             modifier = Modifier
                 .fillMaxWidth()
                 .padding(16.dp),
             verticalAlignment = Alignment.CenterVertically
-        ){
+        ) {
             Image(
                 painter = painterResource(id = R.drawable.o),
                 contentDescription = "",
@@ -50,8 +62,28 @@ fun RencanaStudyView(){
                     .clip(shape = CircleShape)
                     .size(50.dp)
             )
+            Spacer(modifier = Modifier.padding(start = 16.dp))
+            Column(modifier = Modifier.weight(1f)) {
+                Text(
+                    text = mahasiswa.nama,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 12.sp,
+                    color = Color.White
+                )
+                Text(
+                    text = mahasiswa.nim,
+                    fontWeight = FontWeight.Light,
+                    fontSize = 12.sp,
+                    color = Color.White
+                )
+                Box{
+                    Icon(
+                        imageVector = Icons.Filled.Notifications,
+                        contentDescription = "",
+                        tint = Color.White
+                    )
+                }
+            }
         }
     }
-
-
 }
