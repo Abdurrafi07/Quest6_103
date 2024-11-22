c001ff746cdab13136b5160fbf1199c888ac1d20
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2024-11-22 19:15:24 +0700

    Membuat File RencanaStudyView

---
+++ app/src/main/java/com/example/p6problemsolving/ui/view/RencanaStudyView.kt
@@ -0,0 +1,12 @@
+package com.example.p6problemsolving.ui.view
+
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+
+@Composable
+fun RencanaStudyView(){
+    var choesenDropdown by remember { mutableStateOf("") }
+}
