6fa49e54237e9b29d7de50df87e59e14a97c51d4
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2024-11-22 21:15:29 +0700

    Menambahkan Row dan RadioButton

--- app/src/main/java/com/example/p6problemsolving/ui/view/RencanaStudyView.kt
+++ app/src/main/java/com/example/p6problemsolving/ui/view/RencanaStudyView.kt
@@ -2,6 +2,7 @@ package com.example.p6problemsolving.ui.view
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -14,7 +15,9 @@ import androidx.compose.foundation.shape.CircleShape
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Notifications
+import androidx.compose.material3.HorizontalDivider
 import androidx.compose.material3.Icon
+import androidx.compose.material3.RadioButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
@@ -34,6 +37,7 @@ import androidx.compose.ui.unit.sp
 import com.example.navigasidengandata.ui.widget.DynamicSelectTextField
 import com.example.p6problemsolving.R
 import com.example.p6problemsolving.data.MataKuliah
+import com.example.p6problemsolving.data.RuangKelas
 import com.example.p6problemsolving.ui.model.Mahasiswa
 
 //@Preview(showBackground = true)
@@ -115,6 +119,30 @@ fun RencanaStudyView(
                             choesenDropdown = it
                         }
                     )
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    HorizontalDivider()
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    Text(text = "Pilih Kelas Belajar", fontWeight = FontWeight.Bold)
+                    Text(
+                        text = "Silahkan pilih kelas dari mata kuliah yang anda inginkan",
+                        fontSize = 12.sp,
+                        fontWeight = FontWeight.Light
+                    )
+                    Spacer(modifier = Modifier.padding(8.dp))
+                    Row(
+                        modifier = Modifier.fillMaxWidth(),
+                        horizontalArrangement = Arrangement.SpaceEvenly
+                    ) {
+                        RuangKelas.kelas.forEach{ data ->
+                            Row(verticalAlignment = Alignment.CenterVertically) {
+                                RadioButton(
+                                    selected = pilihanKelas == data,
+                                    onClick = { pilihanKelas = data}
+                                )
+                                Text(data)
+                            }
+                        }
+                    }
                 }
             }
         }
