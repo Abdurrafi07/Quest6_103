c373956b0d9ca7452a4dab1a720efe016076610d
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2024-11-22 20:56:43 +0700

    Membuat class RencanaStudyViewModel

---
+++ app/src/main/java/com/example/p6problemsolving/viewmodel/RencanaStudyViewModel.kt
@@ -0,0 +1,12 @@
+package com.example.p6problemsolving.viewmodel
+
+import androidx.lifecycle.ViewModel
+import com.example.p6problemsolving.ui.model.RencanaStudi
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+class RencanaStudyViewModel : ViewModel() {
+    private val _krsState = MutableStateFlow(RencanaStudi())
+    val krsStateUi: StateFlow<RencanaStudi> = _krsState.asStateFlow()
+}
