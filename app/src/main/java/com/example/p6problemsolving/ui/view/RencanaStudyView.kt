bbd1199907777cbb8f1893b708cad5e4782e8c8a
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2024-11-22 19:19:04 +0700

    Membuat listData pada File RencanaStudyView

--- app/src/main/java/com/example/p6problemsolving/ui/view/RencanaStudyView.kt
+++ app/src/main/java/com/example/p6problemsolving/ui/view/RencanaStudyView.kt
@@ -8,7 +8,11 @@ import androidx.compose.runtime.setValue
 
 @Composable
 fun RencanaStudyView(){
-    var choesenDropdown by remember { mutableStateOf("") },
-    var checked by remember { mutableStateOf("") },
+    var choesenDropdown by remember { mutableStateOf("") }
+    var checked by remember { mutableStateOf("") }
     var pilihanKelas by remember { mutableStateOf("") }
+
+    val listData: MutableList<String> = mutableListOf(choesenDropdown, pilihanKelas)
+
+
 }
