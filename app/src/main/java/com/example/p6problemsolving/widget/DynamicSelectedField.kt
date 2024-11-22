772cadb4e5d4a67065e440e26d59c7492aba89b4
Abdurrafi07 <abdurrafinurfakh07@gmail.com>
2024-11-22 20:02:06 +0700

    Membuat Fungsi DynamicSelectedTextFied

--- app/src/main/java/com/example/p6problemsolving/widget/DynamicSelectedField.kt
+++ app/src/main/java/com/example/p6problemsolving/widget/DynamicSelectedField.kt
@@ -1,2 +1,28 @@
 package com.example.p6problemsolving.widget
 
+import androidx.compose.material3.ExposedDropdownMenuBox
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DynamicSelectedTextFied(
+    selectedValue: String,
+    options: List<String>,
+    label: String,
+    onValueChangedEvent: (String) -> Unit,
+    modifier: Modifier = Modifier
+){
+    var expanded by remember { mutableStateOf("") }
+
+    ExposedDropdownMenuBox(
+        expanded = expanded,
+        onExpandedChange = { expanded = !expanded },
+        modifier = modifier
+    ){
+
+    }
+}
